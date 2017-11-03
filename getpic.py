9771000f15e54764d4bbea41bfb686eb1ced041e
Shubhangi Pandey <shubhangi.pandey08@gmail.com>
2017-11-03 23:27:10 +0530

    Add files via upload
    Web scraping to pull some information about anyone on twitter, facebook or any social networking site.
    In this script, I have tried with twitter account of Barack Obama.

---
+++ getpic.py
@@ -0,0 +1,20 @@
+import urllib
+import urllib.request
+from bs4 import BeautifulSoup
+
+url= "https://twitter.com/BarackObama"
+page= urllib.request.urlopen(url)
+soup = BeautifulSoup(page, "html.parser")
+
+print(soup.title.text)
+"""
+for link  in soup.findAll('a'):
+	#print(link.get('href'))
+	print(link.text)
+"""
+print(soup.find('div', {"class":"ProfileHeaderCard"}).find('p').text)
+i=1
+for tweets in soup.findAll('div',{"class":"content"}):
+	print(i)
+	print(tweets.find('p').text)
+	i+=1
