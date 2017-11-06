dd8ccbe7984f13fff2ae3b8e8e0a0e1f9b6ee81b
Shubhangi Pandey <shubhangi.pandey08@gmail.com>
2017-11-06 23:47:33 +0530

    Add files via upload

---
+++ downloadpic.py
@@ -0,0 +1,32 @@
+import urllib
+import urllib.request
+from bs4 import BeautifulSoup
+import os
+
+def make_soup(url):
+	page= urllib.request.urlopen(url)
+	soupdata = BeautifulSoup(page, "html.parser")
+	return soupdata
+"""
+print("Enter the term to be searched")
+term= input()
+"""
+
+soup = make_soup("https://uwaterloo.ca")
+for img in soup.findAll('img'):
+	temp=img.get('src')
+	if(temp[:1]=="/"):
+		image= "https://uwaterloo.co"+temp
+	else:
+		image= temp
+
+	nametemp= img.get('alt')
+	if len(nametemp)==0:
+		filename=str(i)
+		i=i+1
+	else:
+		filename= nametemp
+
+	imagefile= open(filename+".jpeg", 'wb')
+	imagefile.write(urllib.request.urlopen(image).read())
+	imagefile.close()
